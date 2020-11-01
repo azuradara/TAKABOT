6f5a39c31c43d8f3ebe25862a6c6ac2404066e14
Dara <dara-@outlook.jp>
2020-11-01 07:07:08 +0100

    Update takabot.js

--- takabot.js
+++ takabot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+new token = process.env.TOKEN
 
 client.on('ready', () => {
     console.log('TAKA_client ready!')
