0e353916c978caf12808c9b49e4843a4bb24b2ac
Dara <dara-@outlook.jp>
2020-11-01 09:28:13 +0100

    rolemessage

--- takabot.js
+++ takabot.js
@@ -4,19 +4,22 @@ const client = new Discord.Client()
 
 const config = require('./config.json')
 const command = require('./command')
+const roleMessage = require('./role-message')
+
+const roleEmbed = new Discord.MessageEmbed()
+  .setColor('#FFFFFF')
+  .setTitle('Role select')
+  .setFooter('TAKA_/高')
+  .setDescription('React to an emote to get the role associated with it.\n\nMembership in cut gang is not guaranteed by the role, you must first be judged by the enitrety of the cut gang council on your mettle, and then send a picture of your pp next to a piece of paper with your signature to prove that **The Cut One**:tm: does in fact pertain to you.')
+  .AddField("<:amongus:772371006957944842>", "**Among Us**", true)
+  .AddField("<:raycon:772371525180325928>", "**Raycon Homies**", true)
+  .AddField("✂️", "**Cut Gang**", true);
+
 
 client.on('ready', () => {
     console.log('TAKA_client ready!')
 
-    command(client, ['ping', 'test'], (message) => {
-        message.channel.send('Pong!')
-    })
-
-    command(client, 'servers', message => {
-        client.guilds.cache.forEach(guild => {
-            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`)
-        })
-    })
+    roleMessage(client, '772376275544309800', roleEmbed, ['772371006957944842', '772371525180325928', '✂️'])
 
     command(client, 'status', message => {
         const content = message.content.replace('tk/status ', '')
