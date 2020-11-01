48ca3206d2c34e36270d22fb2d696498234c13f8
Dara <dara-@outlook.jp>
2020-11-01 09:30:05 +0100

    async fix

--- role-message.js
+++ role-message.js
@@ -6,7 +6,7 @@ const addReactions = (message, reactions) => {
   }
 }
 
-module.exports = (client, id, text, reactions = []) => {
+module.exports = async (client, id, text, reactions = []) => {
   const channel = await client.channels.fetch(id)
 
   channel.message.fetch().then((messages) => {
