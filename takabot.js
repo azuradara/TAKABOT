e0adbab3377a2abb04f8ac96c0c04511f253d2d8
Dara <dara-@outlook.jp>
2020-11-01 11:20:19 +0100

    presence back

--- takabot.js
+++ takabot.js
@@ -11,6 +11,18 @@ client.on('ready', () => {
     console.log('高TAKA_client ready!')
   
     roleClaim(client)
+
+    command(client, 'status', (message) => {
+      const content = message.content.replace('!status ', '')
+      // "!status hello world" -> "hello world"
+  
+      client.user.setPresence({
+        activity: {
+          name: content,
+          type: 0,
+        },
+      })
+    })
   })
 
 client.login(process.env.TOKEN)
