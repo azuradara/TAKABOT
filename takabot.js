5bdb1c4c8c690ded67cb5a2a7a7cf9e7ca5ce403
Dara <dara-@outlook.jp>
2020-11-01 07:21:38 +0100

    Basic Pong

--- takabot.js
+++ takabot.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+
 const config = require('./config.json')
+const command = require('./command')
 new token = process.env.TOKEN
 
 client.on('ready', () => {
     console.log('TAKA_client ready!')
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(process.env.TOKEN)
