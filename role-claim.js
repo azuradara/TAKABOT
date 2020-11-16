1093d3b5e675872bf9a9111454d1fb3a79257f8e
Dara <dara-@outlook.jp>
2020-11-16 01:36:42 +0100

    Update role-claim.js

--- role-claim.js
+++ role-claim.js
@@ -10,7 +10,8 @@ module.exports = client => {
         amongus: 'Among Us',
         raycon: 'Raycon Homies',
         cutgang: 'Cut Gang',
-        popoga: 'Uncut Gang'
+        popoga: 'Uncut Gang',
+        pogggers: 'Phasmo'
     }
 
     const reactions = []
@@ -36,35 +37,35 @@ module.exports = client => {
       if (user.id === '723819104045105172') {
         return
       }
-  
+
       const emoji = reaction._emoji.name
-  
+
       const { guild } = reaction.message
-  
+
       const roleName = emojis[emoji]
       if (!roleName) {
         return
       }
-  
+
       const role = guild.roles.cache.find((role) => role.name === roleName)
       const member = guild.members.cache.find((member) => member.id === user.id)
-  
+
       if (add) {
         member.roles.add(role)
       } else {
         member.roles.remove(role)
       }
     }
-  
+
     client.on('messageReactionAdd', (reaction, user) => {
       if (reaction.message.channel.id === channelId) {
         handleReaction(reaction, user, true)
       }
     })
-  
+
     client.on('messageReactionRemove', (reaction, user) => {
       if (reaction.message.channel.id === channelId) {
         handleReaction(reaction, user, false)
       }
     })
-  }  
+  }
