4be7e5af54b026a6825d427d0271c9da12378819
Dara <70244862+azuradara@users.noreply.github.com>
2021-01-06 15:33:38 +0100

    taka.js cleanup

--- taka.js
+++ taka.js
@@ -1,12 +1,8 @@
-// const Discord = require('discord.js')
-// const chandler = require('wokcommands')
-// const client = new Discord.Client()
 const { Intents, MessageEmbed } = require('discord.js')
 const { prefix, token, ownerID, mongodbpath } = require('./config.json')
 const path = require('path')
 
 const TakaClient = require('./struc/tkclient')
-// const { formatNum } = require('./tkutilities/tkUtil')
 
 const client = new TakaClient({
   owner: ownerID,
@@ -26,40 +22,9 @@ client.registry
     ['reminder', 'Reminder'],
     ['games', 'Games'],
   ])
-  // .registerDefaultCommands({
-  //   help: false,
-  //   commandState: false,
-  //   unknownCommand: false
-  // })
   .registerCommandsIn(path.join(__dirname, 'cmds'))
 
-// client.setProvider(
-//   MongoClient.connect(mongodbpath)
-//     .then((client) => {
-//       return new MongoDBProvider(client, 'taka-db')
-//     }).catch((err) => {
-//       console.error(err)
-//     })
-// )
 
-// client.on('ready', () => {
-//     console.log('高TAKA_client ready!')
-
-//     client.registry
-//       .registerDefaultTypes()
-//       .registerGroups([
-//         ['misc', 'misc commands']
-//       ])
-//       .registerDefaults()
-//       .registerDefaultCommands({
-//         help: false,
-//         ping: false,
-//         commandState: false,
-//         unknownCommand: false
-//       })
-//       .registerTypesIn(path.join(__dirname, 'types'))
-//       .registerCommandsIn(path.join(__dirname, 'cmds'))
-// })
 
 client.on('ready', async() => {
   client.logger.info(`[READY]高TAKA_client ready.`)
@@ -115,24 +80,5 @@ client.on('commandRun', command => {
 });
 
 client.on('commandError', (command, err) => client.logger.error(`[COMMAND:${command.name}]\n${err.stack}`));
-
-
-// client.on('message', async(message) => {
-//   if (!message.author.bot) {
-//     try {
-//       const { body } = await request
-//         .post('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze')
-//         .query({ key: config.googlekey })
-//         .send({
-//             comment: { text: message.content },
-//             languages: ['en'],
-//             requestedAttributes: { TOXICITY: {} }
-//         })
-      
-//       const toxicity = Math.round(body.attributeScores.TOXICITY.summaryScore.value*100)
-//       if (toxicity >= 85) return message.reply('wtf toxic <:bacDaro:782375944655863878>')
-//     } catch (err) {return message.reply(`Ohnae, \`${err.message}\`.`)}
-//   }
-// })
 
 client.login(token)
